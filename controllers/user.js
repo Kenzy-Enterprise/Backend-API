749b593e331647f24f4c8c77e5481b7123c5ac5e
yinmalya <kenmalya2@gmail.com>
2025-04-30 22:04:49 +0000

    update user.js and auth_routes.js to use async/await

--- controllers/user.js
+++ controllers/user.js
@@ -164,17 +164,24 @@ export const resetPassword = async (req, res) => {
 export const upddateUserRole = async (req, res) => {
   const { error, value } = updateValidator.validate(req.body);
   if (error) {
-    return res.status(422).json(error);
+    return res.status(422).json(error.details); 
   }
 
-  const result = await UserModel.findByIdAndUpdate(
-    req.params.id,
-    value,
+  try {
+    const result = await UserModel.findByIdAndUpdate(
+      req.params.id,
+      value,
+      { new: true }
+    );
 
-    { new: true }
-  );
+    if (!result) {
+      return res.status(404).json({ error: "User not found" });
+    }
 
-  res.status(200).json(result);
+    res.status(200).json(result);
+  } catch (err) {
+    res.status(500).json({ error: "Server error" });
+  }
 };
 
 
--- routes/auth_routes.js
+++ routes/auth_routes.js
@@ -10,6 +10,6 @@ const router = Router();
 // Define routes for user registration and login
 router.post("/register", registerUser);
 router.post("/login", loginUser); 
-router.patch('/id', upddateUserRole);
+router.patch('/:id', upddateUserRole);
 
 export default router;  
